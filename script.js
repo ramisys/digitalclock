7e7fe79d74215f6db19766e6f21bcd5bf15b3f6c
ramisys <fatramcezjames@gmail.com>
2025-08-12 22:01:52 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function realTimeClock() {
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById('time').innerHTML = timeString;
+}
+
+realTimeClock();
+
+setInterval(realTimeClock, 1000);
